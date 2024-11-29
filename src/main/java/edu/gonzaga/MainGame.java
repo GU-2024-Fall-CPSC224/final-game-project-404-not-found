233530b99357dc4ce68c97d3ab4fc9b4ce22c115
LiamGetHub <lgfitting@gmail.com>
2024-11-29 15:41:58 -0800

    created test jframe. Now tackeling ui

--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -11,12 +11,23 @@
  * Copyright: 2023
  */
 package edu.gonzaga;
+import javax.swing.*;
+
 
 
 /** Main program class for launching your team's program. */
 public class MainGame {
     public static void main(String[] args) {
-        System.out.println("Hello Team Game");
+        //System.out.println("Hello Team Game");
+
+        JFrame frame = new JFrame("minesweeper");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Click");
+        frame.add(button);
+
+        frame.setVisible(true);
 
 
         // Your code here. Good luck!
