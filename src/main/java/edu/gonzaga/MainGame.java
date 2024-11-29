b2b0c801943c008a7214174bb31533eca694ebed
LiamGetHub <lgfitting@gmail.com>
2024-11-29 15:56:07 -0800

    created hard coded board. refer to this commit for basics

--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -12,14 +12,14 @@
  */
 package edu.gonzaga;
 import javax.swing.*;
-
+import java.awt.*;
 
 
 /** Main program class for launching your team's program. */
 public class MainGame {
     public static void main(String[] args) {
-        //System.out.println("Hello Team Game");
 
+        /*
         JFrame frame = new JFrame("minesweeper");
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,8 +28,31 @@ public class MainGame {
         frame.add(button);
 
         frame.setVisible(true);
+         */
+
+        //set up frame
+        JFrame frame = new JFrame("Minesweeper");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        //set up pannel
+        JPanel gridPanel = new JPanel();
+        gridPanel.setLayout(new GridLayout(10, 10)); // 10x10 grid
 
 
-        // Your code here. Good luck!
+        //add buttons for grid cells
+        //hard coded grid number
+        JButton[][] buttons = new JButton[10][10];
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                buttons[row][col] = new JButton();
+                gridPanel.add(buttons[row][col]);
+            }
+        }
+
+        //make visible
+        frame.add(gridPanel);
+        frame.setVisible(true);
+
     }
 }
